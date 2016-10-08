bbbb0cd5e993c048f3de1cd4d42b30b80cf33386
dcliu95 <danielcliu95@gmail.com>
2016-10-08 16:43:25 -0700

    added scripts. PC is Useless dont use

---
+++ Assets/Dan/realJump.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class realJump : MonoBehaviour {
+
+    // Use this for initialization
+    Vector2 movementDirection = Vector2.up;
+    public float forceMultiplier = 10;
+
+    public float count = 0;
+    Rigidbody2D Player;
+    bool bounce = true;
+    // Use this for initialization
+    void Start()
+    {
+        Player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {     
+           if (bounce)
+            {
+                Player.velocity = new Vector2(Player.velocity.x, 0);
+            Player.velocity = Player.velocity + movementDirection * forceMultiplier;
+           // print("hit space");
+          //  print(count);
+            //count += 1;
+
+              //  bounce = false;
+            }
+            //bounce = true;
+        }
+    }
+}
